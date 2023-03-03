a093b54e1de88a602b7e161b8a02d9f9472d899b
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-04 03:15:55 +0700

    fix home component

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -9,54 +9,49 @@ import vball from '../images/vball.png'
 import { NavLink } from 'react-router-dom'
 
 
-const Home =()=>{
+const Home = () => {
     return (
-        <div>
-          
-            <ul>
+        <div className="d-flex align-items-center justify-content-center">
+            <ul style={{ "padding-left": "4.5em", "padding-right": "2em" }}>
                 <NavLink activeclassname="active" to="/bad">
                     <div class="box">
-                        <img src={bad} alt=""/>
+                        <img src={bad} alt="" />
                     </div>
                 </NavLink>
 
                 <NavLink activeclassname="active" to="/bball">
                     <div class="box">
-                        <img src={bball} alt=""/>
+                        <img src={bball} alt="" />
                     </div>
                 </NavLink>
 
                 <NavLink activeclassname="active" to="/fball">
                     <div class="box">
-                        <img src={fball} alt=""/>
-                    </div>  
+                        <img src={fball} alt="" />
+                    </div>
                 </NavLink>
-                
-            </ul>
-            <ul>
+
                 <NavLink activeclassname="active" to="/pong">
                     <div class="box">
-                        <img src={pong} alt=""/>
+                        <img src={pong} alt="" />
                     </div>
                 </NavLink>
-                
+
                 <NavLink activeclassname="active" to="/ten">
                     <div class="box">
-                        <img src={ten} alt=""/>
+                        <img src={ten} alt="" />
                     </div>
                 </NavLink>
-                
+
                 <NavLink activeclassname="active" to="/vball">
                     <div class="box">
-                        <img src={vball} alt=""/>
+                        <img src={vball} alt="" />
                     </div>
                 </NavLink>
-                
-                
             </ul>
 
-        </div>
-        
+        </div >
+
     );
 }
 
