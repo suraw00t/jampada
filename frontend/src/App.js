474bb6aa71f6f62b8ee71331ecad062742b2d69c
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-19 05:23:14 +0700

    add refresh token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,26 +16,33 @@ import { Register } from './page/Register';
 
 import Nav from './page/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { refreshToken } from './redux/actions/authAction';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(refreshToken())
+  }, [dispatch])
   return (
     <div className="App">
       <Nav />
       <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/bad' element={<Bad/>} />
-          <Route path='/bball' element={<Bball/>} />
-          <Route path='/fball' element={<Fball/>} />
-          <Route path='/vball' element={<Vball/>} />
-          <Route path='/pong' element={<Pong/>} />
-          <Route path='/ten' element={<Ten/>} />
-          <Route path='/Signin' element={<Signin/>}/>
-          <Route path='/Register' element={<Register/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/badminton' element={<Bad />} />
+        <Route path='/basketball' element={<Bball />} />
+        <Route path='/football' element={<Fball />} />
+        <Route path='/volleyball' element={<Vball />} />
+        <Route path='/pingpong' element={<Pong />} />
+        <Route path='/tennis' element={<Ten />} />
+        <Route path='/Signin' element={<Signin />} />
+        <Route path='/Register' element={<Register />} />
 
       </Routes>
-  
-      
+
+
     </div>
   );
 }
