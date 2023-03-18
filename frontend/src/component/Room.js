9dc24650320c30e33630fefae3a57894a277e847
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-19 05:24:33 +0700

    add http method

--- frontend/src/component/Room.js
+++ frontend/src/component/Room.js
@@ -1,82 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Room.css';
 import { Modal } from './Modal';
 import Button from 'react-bootstrap/Button';
+import { useLocation, useNavigate } from "react-router-dom"
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-export const Room = () => {
+
+export const Room = ({ response }) => {
+  const { pathname } = useLocation();
+  async function handleDelete(topic_id) {
+    console.log(topic_id)
+    await axios.delete("http://localhost:8000/api/v1/topic/delete?topic_id=" + topic_id)
+    window.location.reload()
+  }
+
+  const [edit, setEdit] = useState([])
+  async function handleEdit(topic_id, topic_type) {
+    const response = await axios.get("http://localhost:8000/api/v1/topic/" + topic_type + "/" + topic_id)
+    setEdit(response.data)
+  }
+  const resetForm = () => {
+    setEdit("")
+  }
+
+  const { auth } = useSelector(state => state)
+  const navigate = useNavigate()
+  // const location = useLocation();
+  useEffect(() => {
+    if (!auth.token) navigate("/Signin");
+    // const referer = location.state && location.state.referer
+
+
+  }, [auth.token, navigate])
   return (
-    <div className='content'>
-      <Modal />
+    < div className='content' >
+      <Modal typename={pathname.replace("/", "")} data={edit} />
       <div className='card'>
+        {
+          response?.length > 0 && response.map(it => (
+            // <h1 key={it.id}>{it.id}</h1>
+            <div className='roombox' key={it.id}>
+              <div className='text-white'>
+                <h5><span className='text-warning text-uppercase'>{it.name}</span></h5>
+                <p>Level: {it.level}</p>
+                <p>Player: {it.player}</p>
+                <p>Time: {it.date_time}</p>
+                <p>Place: {it.place}</p>
+                <p>Detail: {it.detail ? it.detail : "-"}</p>
+              </div>
+              <div className='setButton d-flex justify-content-between'>
+                <button
+                  type='button'
+                  className='btn btn-success'
+                  data-toggle='modal'
+                  data-target='#exampleModal'
+                  onClick={() => handleEdit(it.id, it.type)}
+                >
+                  Edit
+                </button>
+                <Button variant='btn btn-danger' onClick={() => handleDelete(it.id)}>Delete</Button>
+              </div>
+            </div>
+          ))
+        }
         <div className='roombox'>
-          <h1>ว่าง</h1>
+          <h1>สร้าง</h1>
           <button
             type='button'
-            className='btn btn-primary'
+            className='btn btn-secondary'
             data-toggle='modal'
             data-target='#exampleModal'
+            onClick={() => resetForm()}
           >
             Post
           </button>
-          <div className='setButton'>
-            <Button variant='outline-danger'>Delete</Button>
-          </div>
-        </div>
-        <div className='roombox'>
-         <div className='text-white'>
-          <h5>Level: <span className='text-warning text-uppercase'>Beginner</span></h5>
-          <p>Player: 5</p>
-          <p>Time: 12:14:00</p>
-          <p>Price: 12$</p>
-          <p>Detail: ไม่มี</p>
-         </div>
-          <div className='setButton'>
-            <Button variant='outline-danger'>Delete</Button>
-          </div>
-        </div>
-        <div className='roombox'>
-          <h1>ว่าง</h1>
-          <button
-            type='button'
-            className='btn btn-primary'
-            data-toggle='modal'
-            data-target='#exampleModal'
-          >
-            Post
-          </button>
-          <div className='setButton'>
-            <Button variant='outline-danger'>Delete</Button>
-          </div>
-        </div>
-        <div className='roombox'>
-          <h1>ว่าง</h1>
-          <button
-            type='button'
-            className='btn btn-primary'
-            data-toggle='modal'
-            data-target='#exampleModal'
-          >
-            Post
-          </button>
-          <div className='setButton'>
-            <Button variant='outline-danger'>Delete</Button>
-          </div>
-        </div>
-        <div className='roombox'>
-          <h1>ว่าง</h1>
-          <button
-            type='button'
-            className='btn btn-primary'
-            data-toggle='modal'
-            data-target='#exampleModal'
-          >
-            Post
-          </button>
-          <div className='setButton'>
-            <Button variant='outline-danger'>Delete</Button>
-          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
