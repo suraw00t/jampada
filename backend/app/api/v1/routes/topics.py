668e1d1bbb1b64db985892b05fb08fc8b042a91a
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-19 05:20:59 +0700

    add put method

--- backend/app/api/v1/routes/topics.py
+++ backend/app/api/v1/routes/topics.py
@@ -26,30 +26,50 @@ async def create_topic(topic: TopicCreate):
     return Topic(id=str(new_topic.id), **new_topic.to_mongo().to_dict())
 
 
-@router.get("/all", response_model_by_alias=False, response_model=List[Topic])
+@router.put("/edit/{topic_type}/{topic_id}", response_model=Topic)
+async def edit_topic(topic_type: str, topic_id: str, topic: TopicCreate):
+    topic_db = models.Topic.objects(id=topic_id).first()
+    if not topic_db:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Topic not found")
+    topic_db.level = topic.level
+    topic_db.place = topic.place
+    topic_db.player = topic.player
+    topic_db.detail = topic.detail
+    topic_db.date_time = topic.date_time
+    topic_db.save()
+    return Topic(id=str(topic_db.id), **topic_db.to_mongo().to_dict())
+
+
+@router.get("/all", response_model=List[Topic])
 async def get_all():
     topics = models.Topic.objects.all()
     return [Topic(id=str(topic.id), **topic.to_mongo().to_dict()) for topic in topics]
 
 
-@router.get("/{topic_id}", response_model_by_alias=False, response_model=Topic)
-async def get_single_topic(topic_id: str):
-    try:
-        topic = models.Topic.objects.get(id=topic_id)
-    except DoesNotExist as e:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(e))
-    print(topic.date_time.isoformat())
+@router.get("/{topic_type}", response_model=List[Topic])
+async def get_topic_by_type(topic_type: str):
+    topics = models.Topic.objects(type=topic_type)
+    if not topics:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No topics data")
+    return [Topic(id=str(topic.id), **topic.to_mongo().to_dict()) for topic in topics]
+
+
+@router.get("/{topic_type}/{topic_id}", response_model=Topic)
+async def get_single_topic(topic_type: str, topic_id: str):
+    topic = models.Topic.objects(id=topic_id).first()
+    if not topic:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No topics data")
     return Topic(id=str(topic.id), **topic.to_mongo().to_dict())
 
 
 @router.post("/join", response_model=Topic)
 async def join_topic(topic_id: str, current_user: User = Depends(get_current_user)):
-    print(current_user)
+    print(current_user, topic_id)
     return {"": ""}
 
 
-@router.delete("/delete/{topic_id}", response_model=Topic)  # delete topic
-async def delete_topic(topic_id):
+@router.delete("/delete", response_model=Topic)  # delete topic
+async def delete_topic(topic_id: str):
     try:
         del_topic = models.Topic.objects.get(id=topic_id)
     except DoesNotExist as e:
