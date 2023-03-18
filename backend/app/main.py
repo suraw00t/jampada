492f5e919969f80875471ee9ddece4118c42324c
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-18 17:39:20 +0700

    add gzip and static

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.staticfiles import StaticFiles
 
-from app.api.errors import http_error_handler
-
-from app.api.errors import http422_error_handler
-
+from app.api.errors import http422_error_handler, http_error_handler
 from app.api.v1.routes.api import router as api_router_v1
 from app.core import get_app_settings
 from app.models import init_mongoengine
@@ -23,10 +21,12 @@ def get_application() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
+    application.add_middleware(GZipMiddleware, minimum_size=1000)
     application.add_exception_handler(HTTPException, http_error_handler)
     application.add_exception_handler(RequestValidationError, http422_error_handler)
-
     application.include_router(api_router_v1, prefix=f"{settings.API_PREFIX}/v1")
+    application.mount("/static", StaticFiles(directory="app/static"), name="static")
+
     init_mongoengine(settings)
+
     return application
