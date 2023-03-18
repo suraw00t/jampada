c8212230522312d7f702cacf5de9fcc1fb8584c2
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-19 05:22:33 +0700

    add field

--- backend/app/models/topics.py
+++ backend/app/models/topics.py
@@ -10,6 +10,8 @@ class Topic(me.Document):
     date_time = me.DateTimeField(required=True)
     place = me.StringField(required=True)
     detail = me.StringField(max_length=120)
+    player = me.StringField(required=True, max_length=120)
+    type = me.StringField(required=True, max_length=50)
     # owner = me.ReferenceField("User", dbref=True, required=True)
     created = me.DateTimeField(required=True, default=datetime.now)
     updated = me.DateTimeField(required=True, default=datetime.now, auto_now=True)
