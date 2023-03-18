07797a0c4ba98e99f6fde27bc938e36553b3afc6
Surawut Saithong-in <surawut.kong@gmail.com>
2023-03-18 17:44:42 +0700

    fix error and add exception

--- backend/app/api/v1/routes/topics.py
+++ backend/app/api/v1/routes/topics.py
@@ -1,32 +1,71 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette.status import (
+    HTTP_401_UNAUTHORIZED,
+    HTTP_404_NOT_FOUND,
+    HTTP_409_CONFLICT,
+)
+from mongoengine.errors import DoesNotExist
+import datetime
+from typing import List
 
 from app.schemas import Topic, TopicCreate
 import app.models as models
-import datetime
 
 router = APIRouter()
 
 
-@router.post("/create")
+@router.post("/create", response_model=Topic)
 async def create_topic(topic: TopicCreate):
     new_topic = models.Topic(**topic.dict())
-    new_topic.date_time = datetime.datetime.utcfromtimestamp(int(topic.date_time))
     new_topic.save()
-    return {"message": "Create topic successfull"}
+    return Topic(
+        id=str(new_topic.id),
+        # date_time=str(new_topic.date_time),
+        **new_topic.to_mongo().to_dict()
+    )
 
 
-@router.get("/all", response_model_by_alias=False)
+@router.get("/all", response_model_by_alias=False, response_model=List[Topic])
 async def get_all():
     topics = models.Topic.objects.all()
-    return topics.to_json()
+    return [
+        Topic(
+            id=str(topic.id),
+            date_time=str(topic.date_time),
+            **topic.to_mongo().to_dict()
+        )
+        for topic in topics
+    ]
+
+
+@router.get("/{topic_id}", response_model_by_alias=False, response_model=Topic)
+async def get_single_topic(topic_id: str):
+    try:
+        topic = models.Topic.objects.get(id=topic_id)
+    except DoesNotExist as e:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(e))
+    print(topic.date_time.isoformat())
+    return Topic(
+        id=str(topic.id),
+        date_time=topic.date_time.isoformat(),
+        **topic.to_mongo().to_dict()
+    )
 
 
 @router.post("/join/{user_id}{topic_id}")
 async def join_topic(user_id, topic_id):
     return ""
 
-@router.delete("/delete/{topic_id}") #delete topic
+
+@router.delete("/delete/{topic_id}", response_model=Topic)  # delete topic
 async def delete_topic(topic_id):
-    del_topic = models.Topic.objects.get(id = topic_id)
-    del_topic.delete() 
-    return {"message": "Delete topic successfull"}
+    try:
+        del_topic = models.Topic.objects.get(id=topic_id)
+    except DoesNotExist as e:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(e))
+    del_topic.delete()
+    return Topic(
+        id=str(del_topic.id),
+        date_time=str(del_topic.date_time),
+        **del_topic.to_mongo().to_dict()
+    )
--- backend/app/api/v1/routes/users.py
+++ backend/app/api/v1/routes/users.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, requests
+from fastapi import APIRouter, Depends, HTTPException
 from starlette.status import (
     HTTP_401_UNAUTHORIZED,
     HTTP_404_NOT_FOUND,
     HTTP_409_CONFLICT,
 )
+from mongoengine.errors import DoesNotExist
 
 from app import models
 from app.core import deps
@@ -14,17 +15,19 @@ from loguru import logger
 router = APIRouter()
 
 
-@router.post("/create")
+@router.post("/create", response_model=User)
 async def create_user(user: UserCreate):
     new_user = models.User(**user.dict())
     new_user.set_password(user.password)
     new_user.save()
 
-    return {"message": "Create user successfull"}
+    return User(id=str(new_user.id), **new_user.to_mongo())
 
 
-@router.get("/{user_id}", response_model_by_alias=False)
+@router.get("/{user_id}", response_model_by_alias=False, response_model=User)
 async def get_user(user_id):
-    user = models.User.objects(id=user_id).first()
-
-    return user.to_json()
+    try:
+        user = models.User.objects.get(id=user_id)
+    except DoesNotExist as e:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(e))
+    return User(id=str(user.id), **user.to_mongo())
